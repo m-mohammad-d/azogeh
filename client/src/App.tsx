e0e4e2909612d0435ecaeba801108be48a4f89ce
Hossein Rezaei <rezaeig22@gmail.com>
2024-08-29 20:06:52 +0330

    Client: make a test request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+
+interface FetchResponse<T> {
+  status: string; // Status of the response (e.g., "success")
+  results: number; // Number of results returned
+  pagination?: Pagination; // Pagination details
+  data: Data<T>; // Data containing the products
+}
+
+interface Pagination {
+  total: number; // Total number of items
+  limit: number; // Limit of items per page
+  pages: number; // Total number of pages
+  page: number; // Current page number
+  skip: number; // Number of items skipped
+}
+
+interface Data<T> {
+  products: T[]; // Array of products
+}
+
+interface Product {
+  _id: string; // Unique identifier for the product
+  name: string; // Name of the product
+  description: string; // Description of the product
+  image: string; // Main image of the product
+  images: string[]; // Array of additional images
+  countInStock: number; // Number of items in stock
+  isAvailable: boolean; // Availability status
+  brand: string; // Brand of the product
+  category: string; // Category of the product
+  rating: number; // Product rating
+  numReviews: number; // Number of reviews
+  price: number; // Original price of the product
+  discount: number; // Discount percentage
+  createdAt: string; // Creation date of the product
+  updatedAt: string; // Last update date of the product
+  slug: string; // URL-friendly identifier
+  discountedPrice: number; // Price after discount
+  id: string; // Alternate unique identifier for the product
+}
+
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState<FetchResponse<Product>>();
+
+  useEffect(() => {
+    fetch("/api/products?page=1")
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  return <>{data?.data.products.map(p => <li key={p.id}>{p.name}</li>)}</>;
 };
 
 export default App;
