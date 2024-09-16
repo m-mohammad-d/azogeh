7d2d4f5993e87c9347b082f28065940dfc59c0fd
silvershade1 <silvershade1000@gmail.com>
2024-09-16 21:07:59 +0330

    fix: make bestSaller responsive for mobile and tablet views

--- client/src/components/BestSallersProduct.tsx
+++ client/src/components/BestSallersProduct.tsx
@@ -6,17 +6,17 @@ interface BestSallersProductProps {
 
 function BestSallersProduct({ name, img, price }: BestSallersProductProps) {
   return (
-    <div className="border bg-[#FDFAFB] shadow-md px-4 py-6 flex flex-col items-center text-center rounded-lg transition-shadow duration-300 hover:shadow-lg">
+    <div className="border shadow-lg px-2 lg:px-4 py-6 flex flex-col items-center text-center rounded-lg transition-shadow duration-300 hover:shadow-lg">
       <div className="w-full h-28 mb-2 rounded-md overflow-hidden">
         <img src={`/public/images/${img}`} alt={name} className="w-full h-full object-contain" />
       </div>
       <div className="w-full border-t border-gray-100 pt-4 mt-4">
         <div className="h-12">
-          <p className="text-lg font-semibold line-clamp-2">{name}</p>
+          <p className="text-base xl:text-lg font-semibold line-clamp-2">{name}</p>
         </div>
-        <div className="flex justify-between items-center mt-4 flex-nowrap">
+        <div className="flex flex-col 2xl:flex-row justify-between items-center mt-4 flex-nowrap">
           <p className="text-primary-500 whitespace-nowrap">{price}تومان </p>
-          <button className="bg-primary-500 px-4 py-2 text-[10px] rounded-md shadow-md text-white hover:bg-primary-600 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg whitespace-nowrap">
+          <button className="bg-primary-500 mt-2 2xl:mt-0 px-4 py-2 text-[10px] rounded-md shadow-md text-white hover:bg-primary-600 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg whitespace-nowrap">
             افزودن به سبد خرید
           </button>
         </div>
--- client/src/components/BestSellers.tsx
+++ client/src/components/BestSellers.tsx
@@ -6,10 +6,10 @@ interface BestSellersProps {
 }
 function BestSellers({ products }: BestSellersProps) {
   return (
-    <div className="mt-16">
-      <h2 className="text-3xl">پر فروش ترین ها</h2>
+    <div className="mt-16 mx-4">
+      <h2 className="text-lg md:text-3xl font-semibold mb-4">پر فروش ترین ها</h2>
       <HighlightBar />
-      <div className="grid grid-cols-6 gap-x-5 gap-y-7 mt-10">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 gap-y-6 mt-10">
         {products?.slice(0, 12).map(product => (
           <BestSallersProduct name={product.name} img={product.image} price={product.price} />
         ))}
