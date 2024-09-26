98ccd9e1062028659c233296437ee4e5fb791f74
silvershade1 <silvershade1000@gmail.com>
2024-09-26 13:07:05 +0330

    feat : add products route

---
+++ client/src/pages/ProductList.tsx
@@ -0,0 +1,9 @@
+function ProductList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductList
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AppLayout from "./layout/appLayout";
+import ProductList from "./pages/ProductList";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductList />,
       },
     ],
   },
