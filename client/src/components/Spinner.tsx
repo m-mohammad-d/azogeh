63adff83ceb2eb85cbbaf9f9651ad2cbba087e99
silvershade1 <silvershade1000@gmail.com>
2024-10-03 13:48:47 +0330

    update loader

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,10 +1,69 @@
 function Spinner() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="relative">
-        <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <style>
+        {`
+          .loader {
+            width: 48px;
+            height: 48px;
+            margin: auto;
+            position: relative;
+          }
+          
+          .loader:before {
+            content: '';
+            width: 48px;
+            height: 5px;
+            background: rgba(216, 184, 193, 0.5);
+            position: absolute;
+            top: 60px;
+            left: 0;
+            border-radius: 50%;
+            animation: shadow324 0.5s linear infinite;
+          }
+          
+          .loader:after {
+            content: '';
+            width: 100%;
+            height: 100%;
+            background: #821131; /* Tailwind color primary-100 */
+            position: absolute;
+            top: 0;
+            left: 0;
+            border-radius: 4px;
+            animation: jump7456 0.5s linear infinite;
+          }
+          
+          @keyframes jump7456 {
+            15% {
+              border-bottom-right-radius: 3px;
+            }
+            25% {
+              transform: translateY(9px) rotate(22.5deg);
+            }
+            50% {
+              transform: translateY(18px) scale(1, .9) rotate(45deg);
+              border-bottom-right-radius: 40px;
+            }
+            75% {
+              transform: translateY(9px) rotate(67.5deg);
+            }
+            100% {
+              transform: translateY(0) rotate(90deg);
+            }
+          }
+          
+          @keyframes shadow324 {
+            0%, 100% {
+              transform: scale(1, 1);
+            }
+            50% {
+              transform: scale(1.2, 1);
+            }
+          }
+        `}
+      </style>
+      <div className="loader"></div>
     </div>
   );
 }
