8a21cfe46b62efb2b6eabdcd0f077e69d16aa479
silvershade1 <silvershade1000@gmail.com>
2024-10-09 17:41:01 +0330

    feat : add order history

---
+++ client/src/components/OrderCartItem.tsx
@@ -0,0 +1,32 @@
+// CartItem.tsx
+import React from "react";
+
+interface ProductProps {
+  _id: string;
+  price: number;
+  name: string;
+  image: string;
+  quantity: number;
+}
+interface CartItemProps {
+  product: ProductProps;
+}
+
+const OrderCartItem: React.FC<CartItemProps> = ({ product }) => {
+  return (
+    <div className="flex justify-between items-center border p-4 mb-4 rounded-lg shadow-sm">
+      <div className="flex items-center">
+        <img src={product.image} alt={product.name} className="w-16 h-16 object-cover mr-4" />
+        <div className="text-right">
+          <h2 className="text-lg font-semibold">{product.name}</h2>
+          <p className="text-gray-600">تعداد: {product.quantity}</p>
+        </div>
+      </div>
+      <div className="text-right">
+        <p className="text-lg font-semibold">{product.price} تومان</p>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCartItem;
--- client/src/pages/UserPanel/OrderHistory.tsx
+++ client/src/pages/UserPanel/OrderHistory.tsx
@@ -1,9 +1,19 @@
+import OrderCartItem from "../../components/OrderCartItem";
+
 function OrderHistory() {
-    return (
-        <div>
-            محصولات خریداری شده
-        </div>
-    )
+  const orders = [{ _id: "1", name: "پیتزا", quantity: 1, price: 50000, image: "pizza.jpg" }];
+  return (
+    <div className="max-w-screen-xl mx-auto p-6 border border-gray-100">
+      <h1 className="text-3xl font-bold mb-4 text-right">تاریخچه سفارشات</h1>
+
+      <h3 className="text-lg font-bold mt-4 text-right">محصولات خریداری شده:</h3>
+      <div className="mt-2">
+        {orders.map(product => (
+          <OrderCartItem key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default OrderHistory
+export default OrderHistory;
