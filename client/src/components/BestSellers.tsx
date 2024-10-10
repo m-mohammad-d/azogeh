4f001f18cf9fcee716c2f5e1d7a7fdb690540bdc
silvershade1 <silvershade1000@gmail.com>
2024-10-10 17:21:01 +0330

    feat : add products link in best sallers

--- client/src/components/BestSellers.tsx
+++ client/src/components/BestSellers.tsx
@@ -1,19 +1,31 @@
-
+import { Link } from "react-router-dom";
 import { Product } from "../types/product";
 import BestSallersProduct from "./BestSallersProduct";
 import HighlightBar from "./HighlightBar";
+import { FaArrowLeft } from "react-icons/fa";
 interface BestSellersProps {
   products: Product[] | undefined;
 }
 function BestSellers({ products }: BestSellersProps) {
-  
   return (
     <div className="mt-16 mx-4">
-      <h2 className="text-lg md:text-3xl font-semibold mb-4">پر فروش ترین ها</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg md:text-2xl font-semibold mb-4">پر فروش ترین ها</h2>
+        <Link to="/products" className="flex items-center gap-2 text-lg">
+          <span>تمام محصولات</span>
+          <FaArrowLeft />
+        </Link>
+      </div>
       <HighlightBar />
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 gap-y-6 mt-10">
         {products?.slice(0, 12).map(product => (
-          <BestSallersProduct name={product.name} img={product.image} price={product.price} id={product.id} discount={product.discount} />
+          <BestSallersProduct
+            name={product.name}
+            img={product.image}
+            price={product.price}
+            id={product.id}
+            discount={product.discount}
+          />
         ))}
       </div>
     </div>
