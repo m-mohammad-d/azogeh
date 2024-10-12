e580132b381eef38c6ef53a96894c91b4f1fef90
Hossein <rezaeig22@gmail.com>
2024-10-12 13:25:09 +0330

    feat(userModel): user schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,60 @@
 import { model, Schema } from "mongoose";
+import validator from "validator";
+import { IUser } from "../types";
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email address"],
+    required: [true, "Please enter your email address"],
+  },
+  photo: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  active: {
+    type: Boolean,
+    select: false,
+    default: true,
+  },
+
+  password: {
+    type: String,
+    minLength: 8,
+    select: false,
+    required: [true, "Please enter your password"],
+  },
+  passwordConfirmation: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (this: IUser, value: string): boolean {
+        return value === this.password;
+      },
+      message: "Passwords are not the same!",
+    },
+    required: [true, "Please enter your password confirmation"],
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+  passwordResetToken: {
+    type: String,
+  },
+  passwordResetExpires: {
+    type: Date,
+  },
+});
 
 const User = model("User", userSchema);
 export default User;
