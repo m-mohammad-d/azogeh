b20022950304844fb38dfc8d36fee2d53066250b
silvershade1 <silvershade1000@gmail.com>
2024-10-15 19:19:53 +0330

    feat : add private route

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { RootState } from "../store";
 
 function PrivateRoute() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  const userInfo = useSelector((s: RootState) => s.auth.userInfo);
+
+  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 }
 
 export default PrivateRoute;
