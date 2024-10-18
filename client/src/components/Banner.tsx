d0fbe9695fd1126dec1f38bb161aa38489fa8189
silvershade1 <silvershade1000@gmail.com>
2024-10-18 15:48:06 +0330

    style : update Banner style

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -3,8 +3,8 @@ interface BannerProps {
 }
 function Banner({ img }: BannerProps) {
   return (
-    <div className="w-full">
-      <img src={`/public/${img}.png`} className="w-full" />
+    <div className="mx-4">
+      <img src={`/public/${img}.png`} />
     </div>
   );
 }
