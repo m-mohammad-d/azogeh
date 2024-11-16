95c63c30796880709f2218ac31902b9d1742d485
silvershade1 <silvershade1000@gmail.com>
2024-11-16 20:07:42 +0330

    feat: Update PrivateRoute to fetch user info from server

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,9 +1,8 @@
-import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import { RootState } from "../store";
+import { useGetMeQuery } from "../services/UsersApi";
 
 function PrivateRoute() {
-  const userInfo = useSelector((s: RootState) => s.auth.userInfo);
+  const { data: userInfo } = useGetMeQuery({});
 
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 }
