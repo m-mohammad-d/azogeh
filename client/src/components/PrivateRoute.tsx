d770dc64db9e45dbba45d7da157a8d6d2e80e6ea
silvershade1 <silvershade1000@gmail.com>
2024-11-21 10:40:14 +0330

    feat : add loading in PrivateRoute

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,8 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useGetMeQuery } from "../services/UsersApi";
+import Spinner from "./Spinner";
 
 function PrivateRoute() {
-  const { data: userInfo } = useGetMeQuery({});
+  const { data: userInfo, isLoading } = useGetMeQuery({});
+
+  if (isLoading) return <Spinner />;
 
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 }
