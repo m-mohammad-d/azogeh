9f360d0be377d4b96914f678f45bc4c2d582c2fc
silvershade1 <silvershade1000@gmail.com>
2025-01-27 19:38:33 +0330

    style(ProductDescription): update ProductDescription style  for better UI/UX

--- client/src/components/ProductDescription.tsx
+++ client/src/components/ProductDescription.tsx
@@ -3,9 +3,9 @@ import HighlightBar from "./HighlightBar";
 function ProductDescription({ description }: { description: string }) {
   return (
     <div className="mt-20 mx-3">
-      <h2 className="text-2xl">مشخصات</h2>
+      <h2 className="text-2xl text-primary">مشخصات</h2>
       <HighlightBar />
-      <div className="mx-8 leading-10 text-lg mt-6 text-gray-400">
+      <div className="mx-8 leading-10 text-lg mt-6 text-neutral-gray-8">
         <p>{description}</p>
       </div>
     </div>
