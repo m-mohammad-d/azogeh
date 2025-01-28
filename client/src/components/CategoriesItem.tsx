724012d255e9e9d8f9e016973312f510fa50812d
silvershade1 <silvershade1000@gmail.com>
2025-01-28 12:16:04 +0330

    update CategoriesItem background color

--- client/src/components/CategoriesItem.tsx
+++ client/src/components/CategoriesItem.tsx
@@ -11,7 +11,7 @@ interface CategoriesItemProps {
 function CategoriesItem({ id, name, img, className }: CategoriesItemProps) {
   return (
     <Link to={`/products?category=${id}`}>
-      <div className={cn("flex h-full w-full max-w-48 flex-col items-center rounded-md bg-primary-tint1 text-center shadow-md", className)}>
+      <div className={cn("flex h-full w-full max-w-48 flex-col items-center rounded-md text-neutral-gray-8 border border-neutral-gray-2 bg-white text-center shadow-md", className)}>
         <div className="h-32 w-full bg-white">
           <img src={`/category/${img}.webp`} alt={name} className="py-2 object-contain h-full w-full" />
         </div>
