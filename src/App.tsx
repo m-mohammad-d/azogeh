7c930d9a0e0ad9a8f704b776794df66420379066
silvershade1 <silvershade1000@gmail.com>
2025-02-02 19:11:05 +0330

    delete react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { Toaster } from "react-hot-toast";
-import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
   return (
-    <HelmetProvider>
-      <div>
-        <Toaster />
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <div>
+      <Toaster />
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
