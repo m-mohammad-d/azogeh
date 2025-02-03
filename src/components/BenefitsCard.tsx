1afa34746dce4a0b8b5cd8a0af5fa47a8baa1e85
silvershade1 <silvershade1000@gmail.com>
2025-02-03 17:39:03 +0330

    update BenefitsCard style

--- src/components/BenefitsCard.tsx
+++ src/components/BenefitsCard.tsx
@@ -6,13 +6,14 @@ const items = [
   { name: "سلامت کالا", img: "like" },
   { name: "بهترین قیمت", img: "dollar" },
 ];
+
 function BenefitsCard() {
   return (
-    <div className="w-full bg-neutral-gray-1 mt-16 py-10 rounded-lg">
-      <div className="mx-auto w-1/2" >
-        <div className="grid grid-cols-2 items-center gap-5 lg:grid-cols-4">
-          {items.map((item) => (
-            <BenefitsCardItem name={item.name} img={item.img} />
+    <div className="mt-16 w-full rounded-lg bg-neutral-gray-1 px-4 py-10">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid items-center gap-5 text-center grid-cols-2 lg:grid-cols-4">
+          {items.map((item, index) => (
+            <BenefitsCardItem key={index} name={item.name} img={item.img} />
           ))}
         </div>
       </div>
